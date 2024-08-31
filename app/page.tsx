f04773eca8d3aef5a6517a95742a97a6b425c2ad
ochudi <chukwudi.ofoma@pau.edu.ng>
2024-08-31 17:29:40 +0100

    Add Bento Grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import { FaHome } from "react-icons/fa";
 
-import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import Hero from "@/components/Hero";
+import Grid from "@/components/Grid";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
           { name: "Contact", link: "#contact" },
         ]} />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
