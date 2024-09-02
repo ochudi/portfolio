4f163e304f89ecec2516f45555bbd5fb85d45a49
ochudi <chukwudi.ofoma@pau.edu.ng>
2024-09-02 20:25:01 +0100

    Added changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Chudi's Portfolio",
+  description: "Chukwudi Ofoma's portfolio website",
 };
 
 export default function RootLayout({
