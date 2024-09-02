fb00c7a2d4b3080eac02526b216535b51e06c977
ochudi <chukwudi.ofoma@pau.edu.ng>
2024-09-02 16:56:05 +0100

    Add Work Experience section

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { FloatingNav } from "@/components/ui/FloatingNav";
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Clients />
+        <Experience />
       </div>
     </main>
   );
