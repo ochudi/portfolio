4bae8287be6ca428cd8fe4748431b5371e0713a8
ochudi <chukwudi.ofoma@pau.edu.ng>
2024-11-28 13:31:11 +0100

    Add changes to portfolio site

--- data/index.ts
+++ data/index.ts
@@ -49,7 +49,7 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
+    title: "Currently building a Technical Blog Website",
     description: "The Inside Scoop",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
@@ -72,35 +72,35 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    title: "3D Solar System",
+    des: "A website that allows you explore the solar system with a 3D simulation of the planets using Three.js.",
     img: "/p1.svg",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "/ui.earth.com",
+    link: "/",
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    title: "Video Conferencing App",
+    des: "A video conferencing application. It allows you connect with colleagues and friends.",
     img: "/p2.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "/ui.yoom.com",
+    link: "/",
   },
   {
     id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+    title: "Canva Application",
+    des: "A real SaaS app with AI features and a payments and credits system.",
     img: "/p3.svg",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "/ui.aiimg.com",
+    link: "/",
   },
   {
     id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    title: "Animated Apple Website",
+    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js.",
     img: "/p4.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "/ui.apple.com",
+    link: "/",
   },
 ];
 
@@ -109,31 +109,31 @@ export const testimonials = [
     quote:
       "Working with Chukwudi was a fantastic experience. His depth in both frontend and backend development made our project seamless and efficient. He communicated proactively and always went above and beyond to bring our ideas to life. Highly recommended for any team looking for a dependable developer.",
     name: "Sarah Duru",
-    title: "CEO of BrightPath Solutions",
+    title: "CEO of LinePath Nigeria",
   },
   {
     quote:
       "Chukwudi's expertise in UI/UX design transformed our user experience. His unique approach to problem-solving and thoroughness ensured our platform was user-friendly and visually appealing. His work ethic and dedication to quality are unmatched.",
-    name: "Emeka Osuji",
-    title: "Founder of Connectify Hub",
+    name: "Joshua Nwachukwu",
+    title: "Head of IT, UNIV Nigeria",
   },
   {
     quote:
       "Chukwudi's collaborative spirit and technical knowledge stood out in every phase of development. He worked with us as a true partner, understanding our needs and delivering results that surpassed our expectations. A reliable developer who truly cares about the project’s success.",
-    name: "Tunde Ajayi",
-    title: "CTO of VentureLabs",
+    name: "Ifeoluwa Afolabi",
+    title: "Content Creator",
   },
   {
     quote:
       "Chukwudi brings a refreshing level of enthusiasm and dedication to his work. His skills in front-end technologies and attention to detail allowed us to launch on schedule with a highly polished interface. I couldn’t ask for a better addition to the team.",
-    name: "Amara Udeh",
-    title: "Product Manager at DreamSoft",
+    name: "Nicholas Tali",
+    title: "Owner of Nauvitel Electronics",
   },
   {
     quote:
-      "Having Chukwudi on our project was invaluable. His knowledge of both UI/UX design and software development helped us create a cohesive, effective product. He’s professional, creative, and always delivers quality work.",
-    name: "Bola Akinola",
-    title: "Head of Product at Nexus Digital",
+      "Having Chukwudi on our project was invaluable. His knowledge of both UI/UX design and software development helped us create a cohesive, effective product. He's professional, creative, and always delivers quality work.",
+    name: "Bola Sokoya",
+    title: "CEO of Luxury Biz",
   },
 ];
 
